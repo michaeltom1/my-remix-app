371c416a9b9c93f5d1f2218d8dc364ff9170ee30
michaeltom1 <mic81070@gmail.com>
2024-11-11 16:26:19 +0100

    setup

---
+++ app/routes/Disk.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from '@remix-run/react'
+
+const Disk = () => {
+  return (
+    <div>Disk
+        <Link to={`/`}>Home</Link>
+    </div>
+  )
+}
+
+export default Disk
